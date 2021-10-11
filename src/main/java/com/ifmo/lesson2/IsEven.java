62fad8c2b46c6839dedc75e6e251c315af1b94dd
System Administrator <root@Kirill.local>
2021-10-11 12:33:26 +0300

    HW lesson 2 (not completery_0.10)

--- src/main/java/com/ifmo/lesson2/IsEven.java
+++ src/main/java/com/ifmo/lesson2/IsEven.java
@@ -5,7 +5,6 @@ public class IsEven {
     Создать программу, проверяющую и сообщающую на экран, является ли целое число
     записанное в переменную n, чётным либо нечётным.
      */
-
     public static void main(String[] args) {
         int n = 24;
         System.out.println(isEven(n));
