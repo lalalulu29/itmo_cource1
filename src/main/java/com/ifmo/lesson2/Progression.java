81f8cd59b5f6e18cdd334756b6476a68e4f61e12
System Administrator <root@Kirill.local>
2021-10-11 12:31:53 +0300

    HW lesson 2 (not completery_0.8)

--- src/main/java/com/ifmo/lesson2/Progression.java
+++ src/main/java/com/ifmo/lesson2/Progression.java
@@ -5,7 +5,6 @@ public class Progression {
      Создайте программу, выводящую на экран все четырёхзначные числа последовательности
      1000 1003 1006 1009 1012 1015 ….
      */
-
     public static void main(String[] args) {
         // TODO implement
         for(int i = 1000; i < 10000; i+=3) {
