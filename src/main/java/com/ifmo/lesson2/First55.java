13434091b8c14d85eb773c4482ff0a42be5b308e
System Administrator <root@Kirill.local>
2021-10-11 12:33:55 +0300

    HW lesson 2 (not completery_0.11)

--- src/main/java/com/ifmo/lesson2/First55.java
+++ src/main/java/com/ifmo/lesson2/First55.java
@@ -4,6 +4,7 @@ public class First55 {
     /*
     Создайте программу, выводящую на экран первые 55 элементов последовательности 1 3 5 7 9 11 13 15 17 ….
      */
+
     public static void main(String[] args) {
         int iterationCount = 0;
         for(int i = 1; ; i+= 2) {
