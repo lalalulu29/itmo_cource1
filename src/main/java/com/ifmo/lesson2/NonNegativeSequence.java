d8a02a98a10287a3cd6eb9a7bf8d7225910509d8
System Administrator <root@Kirill.local>
2021-10-11 12:32:39 +0300

    HW lesson 2 (not completery_0.9)

--- src/main/java/com/ifmo/lesson2/NonNegativeSequence.java
+++ src/main/java/com/ifmo/lesson2/NonNegativeSequence.java
@@ -5,6 +5,7 @@ public class NonNegativeSequence {
     Создайте программу, выводящую на экран все неотрицательные элементы
     последовательности 90 85 80 75 70 65 60 ….
      */
+
     public static void main(String[] args) {
         for(int i = 90; i >= 0; i-=5) {
             System.out.println(i);
