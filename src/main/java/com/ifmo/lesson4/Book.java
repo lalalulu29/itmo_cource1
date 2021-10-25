b50a1efa86a3745d0302c7ef6cb2f410e44460a2
System Administrator <root@Kirill.local>
2021-10-25 20:15:10 +0300

    HW lesson 4.15

---
+++ src/main/java/com/ifmo/lesson4/Book.java
@@ -0,0 +1,34 @@
+package com.ifmo.lesson4;
+
+import java.util.Objects;
+
+public class Book {
+    String author;
+    String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author) && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, title);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
