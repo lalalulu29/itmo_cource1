78bbc308f4ed7ecbf78aefd7030eb9e798df08b0
Кирилл <vil.rik@mail.ru>
2021-12-20 14:13:41 +0300

    Working with multithreading optimization

--- src/main/java/com/ifmo/bank/Bank.java
+++ src/main/java/com/ifmo/bank/Bank.java
@@ -1,43 +1,49 @@
 package com.ifmo.bank;
 
+import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Bank {
     private final ConcurrentMap<Long, Account> accMap = new ConcurrentHashMap<>();
+    private final AtomicLong atomicLong = new AtomicLong(1);
+
+    private final Object flag = new Object();
 
 
     public ConcurrentMap<Long, Account> getAccMap() {
         return accMap;
     }
 
-    public Account createAccount(long userId) {
-        Account crAcc = accMap.get(userId);
+    public synchronized Account createAccount() {
+
+        Account crAcc = accMap.get(atomicLong.longValue());
         if (crAcc == null) {
-            synchronized (accMap) {
-                crAcc = new Account(1L, 50, userId, false);
-                accMap.put(crAcc.getUserId(), crAcc);
-            }
+            crAcc = new Account(1L, 50, atomicLong.getAndIncrement(), false);
+            accMap.put(crAcc.getUserId(), crAcc);
         }
         return crAcc;
 
     }
 
-    public void transferMoney(long srcAccId, long dstAccId, double amount) {
+    public void transferMoney(long srcAccId, long dstAccId, double amount){
 
         Account srcContact = accMap.get(srcAccId);
         Account dstContact = accMap.get(dstAccId);
         if (srcContact != null &&
                 dstContact != null) {
             if (srcContact.getAmount() - amount >= 0) {
-                synchronized (srcContact) {
+                synchronized (flag) {
                     synchronized (dstContact) {
-                        if (srcContact.getAmount() - amount >= 0) {
+                        synchronized (srcContact) {
                             srcContact.setAmount(srcContact.getAmount() - amount);
                             dstContact.setAmount(dstContact.getAmount() + amount);
+                            System.out.println("src: " + srcContact.getUserId() + ", dst: " + dstContact.getUserId() + ", amount: " + amount);
                         }
                     }
                 }
+
             }
         }
     }
